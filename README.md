2e3ef81a5745eba26dd83f547e12ff6936b5086b
Allen Hammock <ahammock@mac.com>
2020-05-30 19:18:12 -0700

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Angela-Yu-Web-Bootcamp-2020-CodeSandbox
-Created with CodeSandbox
---
+++ src/components/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+var List = function() {
+  const list = (
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  );
+  return list;
+};
+
+export default List;
